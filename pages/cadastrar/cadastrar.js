0a21d0fb44070e9055a310392ca9c408e60fd01b
Leonardo Aragão <lewoaragao@gmail.com>
2022-10-01 21:50:16 -0300

    funcao de inserir de usuario novo

--- pages/cadastrar/cadastrar.js
+++ pages/cadastrar/cadastrar.js
@@ -1,1 +1,10 @@
+var nome = document.querySelector('#nome')
+var senha = document.querySelector('#senha')
+var repitaSenha = document.querySelector('#repitaSenha')
 var btnCadastrar = document.querySelector('#btnCadastrar')
+
+btnCadastrar.addEventListener('click', function () {
+    DB.transaction(function (tx) {
+        tx.executeSql('INSERT INTO usuarios VALUES (?, ?)')
+    })
+})
